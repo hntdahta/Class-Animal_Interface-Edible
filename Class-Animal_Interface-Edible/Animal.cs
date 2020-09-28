e8cec28caa25ba2a424480e23793756b4dbc9b42
Thanh Dat <datmathoingu1100@gmail.com>
2020-09-28 15:03:54 +0700

    Initial commit

---
+++ Class-Animal_Interface-Edible/Animal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Animal_Interface_Edible
+{
+    public abstract class Animal
+    {
+        public abstract string MakeSound();
+    }
+}
---
+++ Class-Animal_Interface-Edible/Apple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Animal_Interface_Edible
+{
+    public class Apple : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Apple could be slided";
+        }
+    }
+}
